ee1565c0862dac40ad7ad3df77edb794d763071c
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-09-12 13:33:10 +0200

    format bash scripts

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -euo pipefail
+
 java -Dsirius.web.graphql.websocket.allowed.origins="*" \
-     -jar packages/sirius-web-sample-application/target/sirius-web-sample-application-*.jar \
-     --spring.datasource.url=jdbc:postgresql://localhost:5433/sirius-web-db \
-     --spring.datasource.username=dbuser \
-     --spring.datasource.password=dbpwd \
-     --spring.liquibase.change-log=classpath:db/changelog/sirius-web.db.changelog.xml \
-     --spring.profiles.active=dev
+	-jar packages/sirius-web-sample-application/target/sirius-web-sample-application-*.jar \
+	--spring.datasource.url=jdbc:postgresql://localhost:5433/sirius-web-db \
+	--spring.datasource.username=dbuser \
+	--spring.datasource.password=dbpwd \
+	--spring.liquibase.change-log=classpath:db/changelog/sirius-web.db.changelog.xml \
+	--spring.profiles.active=dev
--- scripts/restart-siriusweb-postgresql.sh
+++ scripts/restart-siriusweb-postgresql.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -euo pipefail
+
 docker kill sirius-web-postgres
 docker run -p 5433:5432 --rm --name sirius-web-postgres -e POSTGRES_USER=dbuser -e POSTGRES_PASSWORD=dbpwd -e POSTGRES_DB=sirius-web-db -d postgres
