4a4d2a9ca1a18513c94a78d4b2e8946ec7e97dbc
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-09-12 16:48:41 +0200

    make scripts executable and less prone to initial failure

--- scripts/restart-siriusweb-postgresql.sh
+++ scripts/restart-siriusweb-postgresql.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-docker kill sirius-web-postgres
-docker run -p 5433:5432 --rm --name sirius-web-postgres -e POSTGRES_USER=dbuser -e POSTGRES_PASSWORD=dbpwd -e POSTGRES_DB=sirius-web-db -d postgres
+running_containers=$(docker ps -q)
+
+if echo "$running_containers" | grep 'sirius-web-postgres'; then
+	docker stop sirius-web-postgres
+fi
+
+docker run -p 5433:5432 --rm --name sirius-web-postgres \
+	-e POSTGRES_USER=dbuser \
+	-e POSTGRES_PASSWORD=dbpwd \
+	-e POSTGRES_DB=sirius-web-db \
+	-d postgres
