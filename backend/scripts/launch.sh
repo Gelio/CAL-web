3a4d298b7769aa12349d72ad1eef4e17c38cc2e8
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-11-06 18:30:58 +0100

    refactor(scripts): remove unnecessary arguments from launch.sh
    The arguments are already configured in `application.properties` or
    `application-dev.properties` and do not need to be provided again.

--- backend/scripts/launch.sh
+++ backend/scripts/launch.sh
@@ -10,10 +10,5 @@ cd "$script_dir/.."
 # shellcheck disable=SC2086
 java ${EXTRA_JAVA_ARGS:-} \
 	-jar sirius-web-sample-application/target/sirius-web-sample-application-*.jar \
-	--spring.datasource.url=jdbc:postgresql://localhost:5433/sirius-web-db \
-	--spring.datasource.username=dbuser \
-	--spring.datasource.password=dbpwd \
-	--spring.liquibase.change-log=classpath:db/changelog/sirius-web.db.changelog.xml \
 	--spring.profiles.active=dev \
-	--org.eclipse.sirius.web.features.studioDefinition=true \
 	"$@"
