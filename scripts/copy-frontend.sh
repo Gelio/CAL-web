7e68acf575bf5bd8ed172a0540f14cc03e091683
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-11-06 18:29:58 +0100

    fix(build): support running `copy-frontend.sh` from anywhere
    The `copy-frontend.sh` script will use the correct paths no matter which
    directory it is invoked from.

--- scripts/copy-frontend.sh
+++ scripts/copy-frontend.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mkdir -p backend/sirius-web-frontend/src/main/resources/static
-cp -R frontend/build/* backend/sirius-web-frontend/src/main/resources/static
+# https://stackoverflow.com/a/246128/4874344
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+frontend_destination_directory="$script_dir/backend/sirius-web-frontend/src/main/resources/static"
+
+mkdir -p "$frontend_destination_directory"
+cp -R "$script_dir/frontend/build/*" "$frontend_destination_directory"
