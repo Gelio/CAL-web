1761d8e4cefd66ebc365c47322061082f06b0ff4
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-11-07 15:18:47 +0100

    feat(build): persist database data between restarts
    Use a Docker volume for PostgreSQL data.
    
    This way the data will be persisted through database restarts.

--- scripts/restart-siriusweb-postgresql.sh
+++ scripts/restart-siriusweb-postgresql.sh
@@ -9,9 +9,16 @@ if [ -n "$container_running" ]; then
 	docker stop sirius-web-postgres
 fi
 
+pgdata_volume_name=sirius-web_pgdata
+
+if ! docker volume inspect "$pgdata_volume_name" >/dev/null 2>&1; then
+	docker volume create "$pgdata_volume_name"
+fi
+
 echo "Starting $container_name"
 docker run -p 5433:5432 --rm --name sirius-web-postgres \
 	-e POSTGRES_USER=dbuser \
 	-e POSTGRES_PASSWORD=dbpwd \
 	-e POSTGRES_DB=sirius-web-db \
+	-v $pgdata_volume_name:/var/lib/postgresql/data \
 	-d postgres
