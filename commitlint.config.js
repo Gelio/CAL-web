d547c58d570b380af91dfa5f73f364b1cf5fcd90
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-11-14 20:10:25 +0100

    chore: add commitlint script and GitHub action
    Helps keep the commit messages clean.

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "docs",
+        "experiment",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+    "scope-empty": [1, "never"],
+  },
+};
