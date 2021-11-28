d960b12fc5b77d4ab915e68a00a7ec4c66296f78
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-11-28 20:05:34 +0100

    chore(example-app): add a GH workflow
    Add a workflow that builds the example frontend application.

--- frontend-library-example/src/app.js
+++ frontend-library-example/src/app.js
@@ -4,7 +4,8 @@ const container = document.getElementById("root");
 
 renderApp(
   {
-    authToken: "",
+    authToken:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImRlbW8iLCJzdWIiOiJkZW1vIiwianRpIjoiNTg4NTRjYTYyMWIxNGRiYTgwMDZmNzgyYzU1NDRmNGQiLCJzaWQiOiJjNmI3MmRiOGZhYzI0ZDlhYjliZmNiMTk2NzUyYjQ4ZiIsImV4cCI6MTYzODEzMTM3OSwiaXNzIjoid3V0LmJhbHRpY2xzYy5ldSIsImF1ZCI6Ind1dC5iYWx0aWNsc2MuZXUifQ.b3-PAxFiUxPDdOOGDGaL7NrYr-jmyJmqYuuL2SKIJ_g",
     projectId: "",
   },
   container
