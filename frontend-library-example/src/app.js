1f7007cf55ad707f53d19bd864776cc8c075e2fe
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-11-30 09:54:56 +0100

    feat(example-app): support passing auth token and remounting the app
    Add an input field for the authentication token to be passed in during
    runtime.
    
    Moreover, add a button to unmount and remount the application.

--- frontend-library-example/src/app.js
+++ frontend-library-example/src/app.js
@@ -1,18 +1,45 @@
-import { renderApp } from "@eclipse-sirius/sirius-web";
+// @ts-check
+import { renderApp, unmountApp } from "@eclipse-sirius/sirius-web";
 
 const container = document.getElementById("root");
 
-renderApp(
-  {
-    authToken:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImRlbW8iLCJzdWIiOiJkZW1vIiwianRpIjoiNTg4NTRjYTYyMWIxNGRiYTgwMDZmNzgyYzU1NDRmNGQiLCJzaWQiOiJjNmI3MmRiOGZhYzI0ZDlhYjliZmNiMTk2NzUyYjQ4ZiIsImV4cCI6MTYzODEzMTM3OSwiaXNzIjoid3V0LmJhbHRpY2xzYy5ldSIsImF1ZCI6Ind1dC5iYWx0aWNsc2MuZXUifQ.b3-PAxFiUxPDdOOGDGaL7NrYr-jmyJmqYuuL2SKIJ_g",
-    projectId: "",
-
-    apiURLs: {
-      // NOTE: assumes Sirius Web backend is running locally
-      siriusWebWSAPIURL: "ws://localhost:8080",
-      siriusWebHTTPAPIURL: "http://localhost:8080",
-    },
-  },
-  container
+/** @type {HTMLButtonElement} */
+const toggleApplicationButton = document.getElementById("toggle-application");
+console.assert(
+  toggleApplicationButton,
+  "App explanation container was not found"
 );
+
+/** @type {HTMLInputElement} */
+const authTokenInput = document.getElementById("auth-token");
+console.assert(
+  authTokenInput,
+  "Authentication token input element was not found"
+);
+
+let appMounted = false;
+toggleApplicationButton.addEventListener("click", () => {
+  if (appMounted) {
+    unmountApp(container);
+    toggleApplicationButton.textContent = "Mount application";
+  } else {
+    toggleApplicationButton.textContent = "Unmount application";
+    renderApp(
+      {
+        authToken: authTokenInput.value,
+        projectId: "",
+
+        apiURLs: {
+          // NOTE: assumes Sirius Web backend is running locally
+          siriusWebWSAPIURL: "ws://localhost:8080",
+          siriusWebHTTPAPIURL: "http://localhost:8080",
+        },
+      },
+      container
+    );
+  }
+  appMounted = !appMounted;
+});
+toggleApplicationButton.disabled = false;
+
+toggleApplicationButton.click();
