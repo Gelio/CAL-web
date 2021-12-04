a46fccb3995cde2c0150324a126afb998ba39cb4
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-12-04 20:51:05 +0100

    feat(frontend): show auth-related GraphQL errors toast
    Add a toast notification that is shown when there are
    authentication-related GraphQL errors.
    
    It is not triggered in all cases that the JWT may be invalid. For
    example, for malformed JWTs the 400 status code is returned from the
    backend. This will not trigger the authentication-only toast
    notification.
    
    Closes #103

--- frontend/src/auth/apollo-links.ts
+++ frontend/src/auth/apollo-links.ts
@@ -1,12 +1,20 @@
 import * as O from "fp-ts/lib/Option";
 import { onError } from "@apollo/client/link/error";
+import { useAuthErrorStore } from "./error";
+import { ServerError } from "@apollo/client";
 
 export const getAuthHeaders = (token: O.Option<string>) => ({
   Authorization: O.isSome(token) ? `Bearer ${token.value}` : undefined,
 });
 
 export const getAuthErrorLink = () =>
-  onError(({ graphQLErrors, networkError }) => {
-    console.log({ graphQLErrors, networkError });
-    // TODO: show an error
+  onError(({ networkError }) => {
+    if (networkError && isServerError(networkError)) {
+      if (networkError.statusCode === 401 || networkError.statusCode === 403) {
+        useAuthErrorStore.getState().setError(networkError);
+      }
+    }
   });
+
+const isServerError = (error: Error): error is ServerError =>
+  error.name === "ServerError";
