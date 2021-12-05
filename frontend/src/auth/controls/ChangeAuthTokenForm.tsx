41898ef35b441244d62d23ab2adab03c1a22d72f
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-12-05 18:59:32 +0100

    feat(frontend): add a button to get demo user auth token
    In the auth token modal include a new button that fetches the demo user
    auth token from BalticLSC. Uses the publicly available demo user
    credentials.
    
    Makes trying out CAL-web much simpler, because the user can click a
    single button to continue instead of having to go to BalticLSC and get
    the token from the console.
    
    Closes #100

--- frontend/src/auth/controls/ChangeAuthTokenForm.tsx
+++ frontend/src/auth/controls/ChangeAuthTokenForm.tsx
@@ -1,13 +1,26 @@
 import { Button, styled } from "@material-ui/core";
+import { pipe } from "fp-ts/lib/function";
 import * as O from "fp-ts/lib/Option";
-import { FormEventHandler } from "react";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as T from "fp-ts/lib/Task";
+import {
+  FormEventHandler,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { AuthTokenAcquiryInformation } from "./AuthTokenAcquiryInformation";
+import { APIURLsStore, useAPIURLsStore } from "../../api-urls";
 
 interface ChangeAuthTokenFormProps {
   initialAuthToken: O.Option<string>;
   onSubmit: (authToken: string) => void;
   onCancel: () => void;
 }
+
+const balticLSCAPIUrlSelector = ({ balticLSCAPIURL }: APIURLsStore) =>
+  balticLSCAPIURL;
 
 export const ChangeAuthTokenForm = ({
   initialAuthToken,
@@ -19,21 +32,77 @@ export const ChangeAuthTokenForm = ({
     const formData = new FormData(event.target as HTMLFormElement);
     onSubmit(formData.get("authToken").toString());
   };
+  const balticLSCAPIUrl = useAPIURLsStore(balticLSCAPIUrlSelector);
+  const demoUserToken = useDemoUserToken(balticLSCAPIUrl);
+  const tokenTextareaRef = useRef<HTMLTextAreaElement>();
+  const abortController = useRef<O.Option<AbortController>>(O.none);
+
+  const abort = () => {
+    pipe(
+      abortController.current,
+      O.map((controller) => controller.abort())
+    );
+  };
+
+  useEffect(() => abort, []);
+
+  const loadDemoUserToken = () => {
+    abort();
+    const newController = new AbortController();
+    abortController.current = O.some(newController);
+    const run = pipe(
+      T.fromIO(() => {
+        if (tokenTextareaRef.current) {
+          tokenTextareaRef.current.disabled = true;
+        }
+      }),
+      T.apSecond(demoUserToken.load(newController.signal)),
+      TE.match(
+        (error) => {
+          console.error("Could not fetch demo user token", error);
+        },
+        (token) => {
+          if (tokenTextareaRef.current) {
+            tokenTextareaRef.current.value = token;
+          }
+        }
+      ),
+      T.chainIOK(() => () => {
+        if (tokenTextareaRef.current) {
+          tokenTextareaRef.current.disabled = false;
+        }
+      })
+    );
+
+    run();
+  };
+
   return (
     <form onSubmit={onFormSubmit}>
       <AuthTokenTextarea
         defaultValue={O.toNullable(initialAuthToken)}
         name="authToken"
         placeholder="Enter your authentication token"
+        ref={tokenTextareaRef}
       />
 
       <AuthTokenAcquiryInformation />
 
       <AuthTokenFormButtonsContainer>
         <Button onClick={onCancel}>Cancel</Button>
-        <Button type="submit" variant="contained" color="primary">
-          Submit
-        </Button>
+        <ButtonGroup>
+          <Button onClick={loadDemoUserToken} disabled={demoUserToken.loading}>
+            Get demo user token
+          </Button>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={demoUserToken.loading}
+          >
+            Submit
+          </Button>
+        </ButtonGroup>
       </AuthTokenFormButtonsContainer>
     </form>
   );
@@ -51,3 +120,93 @@ const AuthTokenFormButtonsContainer = styled("div")(({ theme }) => ({
   justifyContent: "space-between",
   marginTop: theme.spacing(2),
 }));
+
+const ButtonGroup = styled("div")(({ theme }) => ({
+  display: "flex",
+  gap: theme.spacing(1),
+}));
+
+const demoUserCredentials = {
+  // NOTE: credentials are publicly available on the login page
+  // @see https://balticlsc.iem.pw.edu.pl/#/
+  username: "demo",
+  password: "BalticDemo",
+};
+interface BalticLSCLoginResponseBody {
+  data: {
+    token: string;
+  };
+  message: string;
+  success: boolean;
+}
+const useDemoUserToken = (balticLSCAPIURL: string) => {
+  const [loading, setLoading] = useState(false);
+  const loginUrl = `${balticLSCAPIURL}/backend/Login`;
+
+  const load = useCallback(
+    (abortSignal: AbortSignal) =>
+      pipe(
+        T.fromIO(() => setLoading(true)),
+        T.chain(() =>
+          TE.tryCatch(
+            () =>
+              fetch(loginUrl, {
+                body: JSON.stringify(demoUserCredentials),
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                method: "POST",
+              }),
+            (reason) => reason as Error
+          )
+        ),
+        maybeInterruptTask(abortSignal),
+        TE.chain((response) => {
+          if (!response.ok) {
+            return TE.left(
+              new Error(
+                `Cannot authenticate as demo user (status code ${response.status})`
+              )
+            );
+          }
+
+          return TE.tryCatch(
+            () => response.json(),
+            (reason) => {
+              console.error(
+                "Cannot parse authentication token response",
+                reason
+              );
+              return new Error("Cannot parse response");
+            }
+          );
+        }),
+        TE.chain((body: BalticLSCLoginResponseBody) => {
+          if (!body.success) {
+            return TE.left(
+              new Error(`Could not authenticate: ${body.message}`)
+            );
+          }
+
+          return TE.right(body.data.token);
+        }),
+        T.chainFirst(() => T.fromIO(() => setLoading(false)))
+      ),
+    [loginUrl]
+  );
+
+  return {
+    loading,
+    load,
+  };
+};
+
+/** Stops task execution if the signal was aborted */
+const maybeInterruptTask = <T extends any>(abortSignal: AbortSignal) =>
+  T.chain((value: T) => {
+    if (abortSignal.aborted) {
+      return T.never;
+    }
+
+    return T.of(value);
+  });
